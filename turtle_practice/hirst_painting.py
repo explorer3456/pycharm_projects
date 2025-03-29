27b696898025a74c65974a9475724a80cd8a162d
Ditto Kim <rlawhdgus34@gmail.com>
2025-03-29 23:41:11 +0900

    python get image and extract 10 common color
    - notice that turtle color is compatible with various tuple type
    - not only circle tuple (244, 200, 194), but also Rgb(r=244, g=200,)
    - hence, tuple minor difference is not that matter when processing
    tuple

--- turtle_practice/hirst_painting.py
+++ turtle_practice/hirst_painting.py
@@ -3,10 +3,38 @@ from turtle import Turtle, Screen
 import random
 import colorgram
 
+my_turtle = Turtle()
+my_turtle.shape("circle")
+my_turtle.color("orange")
+my_turtle.pensize(10)
+my_turtle.speed("fastest")
+
+my_screen = Screen()
+my_screen.colormode(255)
+
 color_obj_list = colorgram.extract("image.jpg", 10)
 
-print(type(color_obj_list))
-print(type(color_obj_list[0]))
+hirst_color_list = []
 
-print(color_obj_list)
-print(color_obj_list[0])
+for c in color_obj_list:
+    hirst_color_list.append(c.rgb)
+
+def get_random_color():
+
+    rgb_list = random.choice(hirst_color_list)
+
+    return rgb_list
+
+def draw_line(turtle_obj):
+    angle = random.randint(1,4);
+    turtle_obj.forward(50)
+
+    # change draw type
+    turtle_obj.right(angle * 90)
+
+    turtle_obj.color(get_random_color())
+
+while True:
+    draw_line(my_turtle)
+
+my_screen.exitonclick()

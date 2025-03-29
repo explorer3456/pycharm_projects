7f0ff96197b74723921a3715b18138b99a166c5c
Ditto Kim <rlawhdgus34@gmail.com>
2025-03-29 21:49:06 +0900

    Python turtle practice. Draw angle shapes
    - change draw function more efficiently

--- turtle_practice/main.py
+++ turtle_practice/main.py
@@ -14,14 +14,14 @@ def get_random_color():
 
 def draw_line(turtle_obj, shape):
     angle = int(360 / shape)
-    turtle_obj.forward(100)
-    turtle_obj.right(angle)
+    for _ in range(1, shape + 1):
+        turtle_obj.forward(100)
+        turtle_obj.right(angle)
 
 for shape_idx in range(3,11):
     R, G, B = get_random_color()
     my_turtle.color(R, G, B)
-    for line in range(1, shape_idx+1):
-        draw_line(my_turtle, shape_idx)
+    draw_line(my_turtle, shape_idx)
 
 my_screen = Screen()
 my_screen.exitonclick()

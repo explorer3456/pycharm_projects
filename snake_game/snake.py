9ac8176c42fc67c58a107364d1966702517cc890
Ditto Kim <rlawhdgus34@gmail.com>
2025-04-03 00:28:15 +0900

    python snake game class creation of snake
    - why do we need snake class? i am not sure
    - but you can see that creating snake is good.
    - using snake class, all bunch of co-ordinate operation is done
    on black box.

---
+++ snake_game/snake.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+class Snake:
+    def __init__(self):
+        self.body_list = []
+
+        for i in range(3):
+            t = Turtle(shape="square")
+            t.color("white")
+            t.penup()
+            self.body_list.append(t)
+
+    def move(self, angle):
+        for i in range(len(self.body_list)-1, -1, -1):
+            new_x = self.body_list[i - 1].xcor()
+            new_y = self.body_list[i - 1].ycor()
+
+            if i > 0:
+                self.body_list[i].goto(new_x, new_y)
+        self.body_list[0].setheading(angle)
+        self.body_list[0].forward(20)
+
+
+
+

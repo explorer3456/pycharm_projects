b71feb2d60eecfce298f645efbeb80dbc6217044
Ditto Kim <rlawhdgus34@gmail.com>
2025-04-03 00:04:17 +0900

    python snake game basic operation
    - snake can move with front snake body.
    - also, you can use screen.tracer() and update()
    to hide during snake body is moving.

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,20 +1,28 @@
 from turtle import Turtle, Screen
-
-t0 = Turtle(shape="square")
-t0.color("white")
-
-t1 = Turtle(shape="square")
-t1.color("white")
-t1.penup()
-t1.goto(-20, 0)
-
-
-
+import time
 
 s = Screen()
 s.setup(width=600, height=600)
 s.bgcolor("black")
+s.tracer(0)
 
+t_list = []
+is_game_done = False
 
+for i in range(3):
+    t = Turtle(shape="square")
+    t.color("white")
+    t_list.append(t)
+
+while not is_game_done:
+    for i in range(len(t_list)-1, -1, -1):
+        t_list[i].penup()
+
+        if i > 0:
+            t_list[i].goto(t_list[i-1].xcor(), t_list[i-1].ycor())
+    t_list[0].forward(20)
+    t_list[0].left(90)
+    time.sleep(1)
+    s.update()
 
 s.exitonclick()

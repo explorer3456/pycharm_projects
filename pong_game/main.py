82cdbb61414835111bb16bf84902a942331a2548
Ditto Kim <rlawhdgus34@gmail.com>
2025-04-06 01:44:39 +0900

    python pong game. Add paddle bounce feature
    - note that we didnt implement with precise measure
    - we implement measuring approximate distance with ball and paddle
    - this is quite new approach for me.

--- pong_game/main.py
+++ pong_game/main.py
@@ -18,19 +18,28 @@ user_a = Paddle(350, 0)
 user_b = Paddle(-350, 0)
 ball = Ball()
 
-
 field.onkey(fun=user_a.move_up, key="w")
 field.onkey(fun=user_a.move_down, key="s")
 
 field.onkey(fun=user_b.move_up, key="t")
 field.onkey(fun=user_b.move_down, key="g")
 
+def is_ball_hit_wall(b_obj):
+    return b_obj.ycor() > 270 or b_obj.ycor() < -270
+
+def is_ball_hit_paddle(b_obj, paddle):
+    return b_obj.xcor() > 320 and b_obj.distance(paddle) < 50
+
 while not is_game_done:
     field.update()
 
     ball.move()
 
-    if ball.ycor() > 270:
+    if is_ball_hit_wall(ball):
+        ball.bounce()
+
+    if is_ball_hit_paddle(ball, user_a) or is_ball_hit_paddle(ball, user_b):
+        ball.direction *= -1
         ball.bounce()
 
     time.sleep(0.25)

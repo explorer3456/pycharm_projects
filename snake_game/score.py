71f30ecb1b4173b4e0030ac4f83b8ddfb7fce718
Ditto Kim <rlawhdgus34@gmail.com>
2025-04-06 23:07:14 +0900

    python snake game with saving highest score
    - the highest score is saved in file, and next time game is on,
    the saved highest score is retrieved.

--- snake_game/score.py
+++ snake_game/score.py
@@ -9,6 +9,9 @@ class Score(Turtle):
         self.goto(0, 280)
         self.score = 0
         self.max_score = 0
+        with open("data.txt", mode="r") as data:
+            self.max_score = int(data.read())
+
         self.display_score()
 
     def display_score(self):
@@ -24,6 +27,8 @@ class Score(Turtle):
         self.write("GAME OVER")
         if self.score > self.max_score:
             self.max_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.max_score))
 
     def update_score(self):
         self.score += 1

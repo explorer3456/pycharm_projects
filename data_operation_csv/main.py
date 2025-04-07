6864651755e114b1ab44c0099835b555441964be
Ditto Kim <rlawhdgus34@gmail.com>
2025-04-07 22:12:30 +0900

    python using Panda library. basic panda usage
    - panda data structures consist of two major structure
    - data frame(whole data), and data series(row data)
    - also, you can see various panda method.
    - you can see that using panda, many code operation can be optimized.

--- data_operation_csv/main.py
+++ data_operation_csv/main.py
@@ -1,4 +1,6 @@
 import csv
+import pandas
+from numpy.ma.extras import average
 
 # legacy file opeartion for CSV
 with open("weather_data.csv") as file:
@@ -16,7 +18,31 @@ with open("weather_data.csv") as file:
         if row[1] != "temp":
             temperature.append(int(row[1]))
 
-print(temperature)
+    # print(temperature)
 
+# how to read CSV file using panda
+data = pandas.read_csv("weather_data.csv")
 
+# Panda data frame data. Data frame means whole data
+print(type(data))
+print(data)
+
+# Panda data serires data. Data serires means certain row of data
+print(type(data["temp"]))
+print(data["temp"])
+
+# convert data serires into data list.
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+avg_temp = average(temp_list)
+print(avg_temp)
+
+# Panda data series object support mean,max meathod.
+print(data["temp"].mean())
+print(data["temp"].max())
+
+# Panda data series object add row name attribute automatically.
+# data["temp"] is same as data.temp.
+print(data.temp)
 
